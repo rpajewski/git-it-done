6fd901799be47840d1463918c687d63247fa2bf6
Ricky Bobby <richard.pajewski@gmail.com>
2020-10-06 17:34:58 -0500

    pulled issues from API and displayed them on page

---
+++ assets/js/single.js
@@ -0,0 +1,63 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
+var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+var getRepoIssues = function(repo) {
+    // format the github api url
+    var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
+  
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayIssues(data);
+                    console.log(data);
+                });
+            } else {
+                alert("There was a problem with your request!");
+            }
+        })
+        .catch(function(error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then()` method
+            alert("Unable to connect to GitHub");
+    });
+};
+  
+getRepoIssues('twitter/scalding');
